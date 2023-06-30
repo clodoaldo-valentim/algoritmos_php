027669bde78a5d47c07362877fb597386f45531c
Clodoaldo Valentim <37400001+clodoaldo-valentim@users.noreply.github.com>
2023-06-30 00:51:25 -0300

    foreach.php

---
+++ foreach.php
@@ -0,0 +1,15 @@
+//O foreach é uma estrutura de repetição em PHP utilizada para percorrer arrays e coleções de dados. 
+//O objetivo é simplificar a escrita de loops que precisam acessar todos os elementos de uma coleção.
+/*
+<?php
+$cores = ["branco", "azul", "preto", "vermelho"];
+   foreach($cores as $teste){
+    echo $teste."\n";
+   }
+
+*/
+<?php
+$cores = ["branco", "azul", "preto", "vermelho"];
+   foreach($cores as $teste){
+    echo $teste."\n";
+   }
