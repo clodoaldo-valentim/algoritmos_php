e90e17a42ec81a0973f15d25c6ad8cff8268317d
Clodoaldo Valentim <37400001+clodoaldo-valentim@users.noreply.github.com>
2023-07-01 05:36:53 -0300

    array_push.php

--- array_push.php
+++ array_push.php
@@ -3,6 +3,8 @@ A função array_push() em PHP é utilizada para adicionar um ou mais elementos 
 Ela recebe dois parâmetros: a própria array onde os elementos serão adicionados e o(s) elemento(s) que serão inseridos.
 A sintaxe básica da função array_push() é a seguinte: 
 array_push($array, $elemento1, $elemento2, ...);
+No exemplo abaixo, a função array_push() é utilizada para adicionar o elemento "morango" ao final da array $frutas. 
+O resultado é uma array com os elementos "maçã", "banana", "laranja" e "morango".
 */
 <?php
 $frutas = array("maçã", "banana", "laranja");
